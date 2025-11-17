e6ffdcbf9ccf0ff97eb7848d51bcbd1dfd1ece41
metafosfato <132387617+metafosfato@users.noreply.github.com>
2025-11-17 17:26:02 -0300

    [feat] -  criacao da classe ameaca

---
+++ src/main/java/com/MonitoramentoEspacial/aplicacao/dominio/TipoAmeaca.java
@@ -0,0 +1,9 @@
+package com.MonitoramentoEspacial.aplicacao.dominio;
+
+public enum TipoAmeaca {
+    DETRITOS_ESPACIAIS,
+    TEMPESTADE_SOLAR,
+    ASTEROIDE,
+    RADIACAO_COSMICA,
+    FALHA_COMUNICACAO
+}
