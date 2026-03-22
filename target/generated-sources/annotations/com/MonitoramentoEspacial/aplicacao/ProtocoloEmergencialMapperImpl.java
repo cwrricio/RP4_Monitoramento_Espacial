a0acfaf6d0a5b6f1e38b346f376e998ec78c08e7
Samuel Anthonny Fossari Monteiro <samuelmonteiro.aluno@unipampa.edu.br>
2026-03-22 18:04:42 -0300

    mudanças

--- target/generated-sources/annotations/com/MonitoramentoEspacial/aplicacao/ProtocoloEmergencialMapperImpl.java
+++ target/generated-sources/annotations/com/MonitoramentoEspacial/aplicacao/ProtocoloEmergencialMapperImpl.java
@@ -10,8 +10,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-30T10:21:57-0300",
-    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.44.0.v20251118-1623, environment: Java 21.0.9 (Eclipse Adoptium)"
+    date = "2026-03-22T17:58:53-0300",
+    comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.45.0.v20260224-0835, environment: Java 21.0.10 (Eclipse Adoptium)"
 )
 @Component
 public class ProtocoloEmergencialMapperImpl implements ProtocoloEmergencialMapper {
